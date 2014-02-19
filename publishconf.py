0b93b75f9adab6af05b104dbeef5e4945472bfa8
Ryan Collins <github@ryancollins.org>
2014-02-18 21:29:15 -0500

    Changed publish settings

--- publishconf.py
+++ publishconf.py
@@ -21,6 +21,27 @@ THEME = 'theme/pelican-bootstrap3'
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
+AUTHOR_URL = ('author/{slug}/')  
+AUTHOR_SAVE_AS = ('author/{slug}/index.html')
+
+CATEGORY_URL = ('category/{slug}/') 
+CATEGORY_SAVE_AS = ('category/{slug}/index.html')  
+TAG_URL = ('tag/{slug}/') 
+TAG_SAVE_AS = ('tag/{slug}/index.html')
+
+PAGE_URL = ('{slug}/')
+PAGE_SAVE_AS = ('{slug}/index.html')
+
+#FEED_RSS = 'feed/index.html'
+FEED_ALL_RSS = 'feed/index.rss'
+CATEGORY_FEED_RSS = 'feed/category/%s.rss'
+TAG_FEED_RSS = 'feed/tag/%s.rss'
+
+
+
 DELETE_OUTPUT_DIRECTORY = True
 STATIC_PATHS = [
             'extra/CNAME',
