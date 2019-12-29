50fc4ca0b5be55b13a9914d2b8f92f9ca2d8ae5e
Ryan 'Gozar' Collins <ryan@ryancollins.org>
2019-12-29 16:47:27 -0500

    Setting up a new theme

--- publishconf.py
+++ publishconf.py
@@ -14,7 +14,7 @@ SITEURL = 'https://gtia.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+#CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
